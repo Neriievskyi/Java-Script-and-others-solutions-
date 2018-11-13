7dd70a1dca2051ebbf7fd368bc61cf286b100fff
Neriievskyi Anton <a.nikkulin@gmail.com>
2018-11-13 12:14:41 +0200

    Added new tasks

---
+++ my_java.js
@@ -0,0 +1,26 @@
+
+
+//Celsius to Fahrenheit convertation.
+//Formula:
+//Fahrenheit = Celsius * 9/5 + 32;
+
+//function convertToF(celsius) {
+//      var fahrenheit;
+//      fahrenheit = celsius * 9 / 5 + 32;
+//      return fahrenheit;
+//  }
+
+
+//var str = `umbrella`, i, size = str.length - 1, newStr = ``;
+
+
+//1. Reverse string via `for` loop.
+
+//for(i = size; i >= 0; i -= 1;){
+// newStr += str[i];
+// }
+
+//2. Reverse string via native array method `reverse`.
+
+//str.split(``).reverse().join(``);
+
